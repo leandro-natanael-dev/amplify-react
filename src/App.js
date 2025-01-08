12fd46540b97dc87af92ff7d961a180fd8d7eccf
leandro-natanael-dev <leandro.almiron.dev@gmail.com>
2025-01-08 10:22:56 -0300

    joda al ema

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Te voy a arrancar a lo fierrazo.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn this 8===D AWS
         </a>
       </header>
     </div>
